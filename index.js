8776015b37228eeb78c883d20a3a0df8a1ce01be
xuld <xuld@vip.qq.com>
2016-04-27 10:04:56 +0800

    导入项目

---
+++ index.js
@@ -0,0 +1,24 @@
+﻿var htmlMinifier = require("html-minifier");
+
+module.exports = function HtmlMinifier(file, options) {
+
+    // 设置默认值。
+    options = Object.assign({
+        collapseWhitespace: true,
+        removeComments: true,
+        minifyJS: true,
+        minifyCSS: true
+    }, options);
+
+    // 生成。
+    try {
+        file.content = htmlMinifier.minify(file.content, options);
+    } catch (e) {
+        file.error({
+            plugin: HtmlMinifier.name,
+            error: e,
+            message: e.message
+        });
+    }
+
+};
